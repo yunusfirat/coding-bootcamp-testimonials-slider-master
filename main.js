e90b664e2af2dde77b783f4c02513d9e3bda7d36
yunusfirat <yunusfirat44@gmail.com>
2021-09-04 02:03:41 +0100

    project has been completed

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ const slides = document.querySelectorAll('.slide');
 
 let index = 0;
 display(index);
-function display (index) {
+function display(index) {
 	slides.forEach((slide) => {
 		slide.style.display = 'none';
 	});
 	slides[index].style.display = 'flex';
 }
 
-function nextSlide () {
+function nextSlide() {
 	index++;
 	if (index > slides.length - 1) {
 		index = 0;
 	}
 	display(index);
 }
-function prevSlide () {
+function prevSlide() {
 	index--;
 	if (index < 0) {
 		index = slides.length - 1;
